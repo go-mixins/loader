14c8eb59c93e873271bc5619ca6c2f08f59afa23
Andrew Rodionoff <andviro@gmail.com>
2017-12-19 12:30:36 +0300

    added Changes channel

--- env/loader.go
+++ env/loader.go
@@ -11,19 +11,33 @@ import (
 
 // Loader implements loader.Loader
 type Loader struct {
-	prefix string
+	prefix  string
+	changes chan struct{}
 }
 
 var _ loader.Loader = (*Loader)(nil)
 
 // Load loads the target from environment
 func (l *Loader) Load(dest interface{}) error {
-	return errors.Wrap(envconfig.Process(l.prefix, dest), "unmarshal yml")
+	return errors.Wrap(envconfig.Process(l.prefix, dest), "load from environment")
+}
+
+// Close closes underlying changes channel
+func (l *Loader) Close() error {
+	close(l.changes)
+	return nil
+}
+
+// Changes provides source of config change events. For environment variables
+// there will never be any.
+func (l *Loader) Changes() <-chan struct{} {
+	return l.changes
 }
 
 // New creates loader initialized with environment variable prefix
 func New(prefix string) *Loader {
 	return &Loader{
-		prefix: strings.ToUpper(prefix),
+		prefix:  strings.ToUpper(prefix),
+		changes: make(chan struct{}),
 	}
 }
